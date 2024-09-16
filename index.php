3e8be34f2c3a380d1b982809e050a6d2da54c03b
Ouma Nyang <ouma.nyang1@gmail.com>
2024-09-16 05:43:48 +0300

    [UPDATE] updated font to apple san-fransisco font

--- index.php
+++ index.php
@@ -4,28 +4,12 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>NIDA VERIFICATION</title>
-
-  <!-- Font: Inter -->
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
-
+ 
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
-  <style>
-    body {
-      font-family: "Inter", sans-serif;
-      background: linear-gradient(to right, rgba(255, 250, 250, 0.7), rgba(123, 200, 254, 0.8)), 
-                  url('assets/images/tz-flag-logo6789.jpg') no-repeat center center;
-      background-size: cover;
-      background-attachment: fixed;
-    }
-    p{
-        font-size: .9em;
-        
-    }
-  </style>
+ 
+  <link rel="stylesheet" href="assets/css/app.styles.css">
+  
 </head>
 <body>
   <div class="container py-5 gy-4">
@@ -64,9 +48,9 @@
   </div>
 
   <!-- jQuery -->
-  <script src="assets/js/jquery.min.js"></script>
+  <script src="assets/js/jquery@3.7.1.min.js"></script>
   <!-- jQuery Mask Plugin -->
-  <script src="assets/js/jquery.mask.min.js"></script>
+  <script src="assets/js/jquery.mask.min.js@1.14.16.js"></script>
   <!-- Bootstrap JS -->
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
